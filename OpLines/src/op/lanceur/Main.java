fdd85518d50e62bcb4a982bc851942c7a7135acc
sbosquet <>
2016-05-09 11:34:13 +0200

    Main class corrections

--- OpLines/src/op/lanceur/Main.java
+++ OpLines/src/op/lanceur/Main.java
@@ -3,7 +3,7 @@ package op.lanceur;
 import op.vue.*;
 
 public class Main {
-    public static void main(){
+    public static void main(String [ ] args){
         System.out.println("Main de PrgParra");
         IHMMenu MonIHM = new IHMMenu();
         MonIHM.setVisible(true);
